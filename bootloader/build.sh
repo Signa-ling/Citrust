e4a1873c5ae239671e0e282d33ffacae70d21851
SPICA\トロ <toro970919@gmail.com>
2021-07-15 18:41:45 +0000

    Minor modifications to the build

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd `dirname $0`
 cargo +nightly build -Zbuild-std=core --target x86_64-unknown-uefi --release
--- kernel/build.sh
+++ kernel/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd `dirname $0`
 cargo +nightly build -Zbuild-std=core --target x86-64-CitrustOS.json --release
