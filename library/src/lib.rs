3028f1fe2b56614e8ff4b8466252106a4f567156
SPICA\トロ <toro970919@gmail.com>
2021-07-15 18:27:35 +0000

    Definition of frame buffer configuration info

---
+++ library/src/lib.rs
@@ -0,0 +1,16 @@
+#![no_std]
+
+#[derive(Debug, Clone, Copy)]
+pub enum PixelFormat {
+    kPixelRGBResv8bitPerColor,
+    kPixelBGRResv8bitPerColor,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct FrameBufferConfig {
+    pub frame_buffer: *mut u8,
+    pub pixels_per_scan_line: u32,
+    pub horizontal_resolution: u32,
+    pub vertical_resolution: u32,
+    pub pixel_format: PixelFormat,
+}
